8412bb8704577aa1b25eb2fe376c7b2a0010f358
mateus <leitemcs@hotmail.com>
2024-03-23 10:30:06 -0300

    removed node modules

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify';
+import { routes } from './routes/routes';
+import cors from '@fastify/cors';
+import mysql from '@fastify/mysql';
+
+export const app = fastify();
+app.register(routes);
+app.register(cors, {});
+app.register(mysql, {
+    connectionString: 'mysql://mateus:3003@localhost:3306/agendai'
+});
+
+app.listen({port: 3000}, (error, address) => {
+    if(error){
+        console.log(error);
+    }
+
+    console.log('Server rodando na 3000')
+})
