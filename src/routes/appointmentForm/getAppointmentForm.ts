7aa82119cf429d40e5244cc1b147e1696da842b4
mateus <leitemcs@hotmail.com>
2024-04-01 23:10:41 -0300

    teste compilando sem chamar mysql query

--- src/routes/appointmentForm/getAppointmentForm.ts
+++ src/routes/appointmentForm/getAppointmentForm.ts
@@ -3,11 +3,11 @@ import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 export const getAppointmentForms = async ( app: FastifyInstance) => {
     app.get('/getAppointmentForms', (request: FastifyRequest, response: FastifyReply) => {
         //@ts-ignore
-        app.mysql.query(
+/*         app.mysql.query(
             "select a.*, (select count(id) from items_appointment_form where appointment_form = a.id) as numberOfQuestions from appointment_form as a",
             function onResult(error : any, result : any){
                 response.send(error || result);
             }
-        )
+        ) */
     })
 }
