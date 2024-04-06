8fa1823a24a671caabcdfc044c64e47775d7408b
mateus <leitemcs@hotmail.com>
2024-04-06 15:25:07 -0300

    corringido query insert user

--- src/routes/users/saveUser.ts
+++ src/routes/users/saveUser.ts
@@ -6,10 +6,10 @@ export const saveUser = async ( app: FastifyInstance) => {
     app.post('/saveUser', (request: FastifyRequest<{ Body: User }>, response: FastifyReply) => {
         const requestBody = request.body;
         const password = createHash('md5').update(requestBody.password).digest("hex");
-        console.log(requestBody);
+        console.log(requestBody, password);
         //@ts-ignore
         app.mysql.query(
-            `insert into items_appointment_form (name, email, phone, password) values (${requestBody.name}, ${requestBody.email}, ${requestBody.phone}, ${password})`,
+            `insert into user (name, email, phone, password) values ('${requestBody.name}', '${requestBody.email}', '${requestBody.phone}', '${password}')`,
             function onResult(error: any, result: any){
                 if(error) return response.send(error);
                 response.send({"status": 200, "mensagem": "Usuário criado com sucesso!!", "userId": result.insertId});
