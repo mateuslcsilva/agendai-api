eeeddcfd5f4b844b501afe89b102cba823f67ed1
mateus <leitemcs@hotmail.com>
2024-04-05 22:05:48 -0300

    a

--- src/routes/appointmentForm/saveAppointmentForm.ts
+++ src/routes/appointmentForm/saveAppointmentForm.ts
@@ -5,26 +5,31 @@ import { SaveAppointmentForm } from "../../../types/saveAppointmentForm";
 export const saveAppointmentForm = async ( app: FastifyInstance) => {
     app.post('/saveAppointmentForm', (request: FastifyRequest<{ Body: SaveAppointmentForm }>, response: FastifyReply) => {
         const requestBody : SaveAppointmentForm = request.body;
-        response.send(requestBody);
-        //@ts-ignore
-        app.mysql.query(
-            "insert into appointment_form (form_name) values ('teste nome form')",
-            function onResult(error : any, result : any){
-                let queryItems = '';
-                requestBody.questions.forEach((question : Question, index: number) => {
-                    queryItems += `('${question.question}', '${question.type.toUpperCase()}', '${result.insertId}')${++index < requestBody.questions.length ? ',' : ''}`
-                })
-                //@ts-ignore
-                app.mysql.query(
-                    `insert into items_appointment_form (question, type, appointment_form) values ${queryItems}`,
-                    function onResult(errorItems: any, resultItems: any){
-                        response.send(errorItems || resultItems);
-                    },
-                    function onError(){
-                        //
-                    }
-                )
-            }
-        )
+        console.log(requestBody);
+        try{
+            //@ts-ignore
+            app.mysql.query(
+                "insert into appointment_form (form_name) values ('teste nome form')",
+                function onResult(error : any, result : any){
+                    let queryItems = '';
+                    requestBody.questions.forEach((question : Question, index: number) => {
+                        queryItems += `('${question.question}', '${question.type.toUpperCase()}', '${result.insertId}')${++index < requestBody.questions.length ? ',' : ''}`
+                    })
+                    //@ts-ignore
+                    app.mysql.query(
+                        `insert into items_appointment_form (question, type, appointment_form) values ${queryItems}`,
+                        function onResult(errorItems: any, resultItems: any){
+                            response.send(errorItems || resultItems);
+                        },
+                        function onError(){
+                            //
+                        }
+                    )
+                }
+            )
+        } catch(error){
+            console.log(error)
+        }
+
     })
 }
