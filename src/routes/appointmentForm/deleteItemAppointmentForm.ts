2d8fa18116eb7a073c65beb5c64c6effa2afb451
mateus <leitemcs@hotmail.com>
2024-04-06 13:10:02 -0300

    corrigido delete items appointment

--- src/routes/appointmentForm/deleteItemAppointmentForm.ts
+++ src/routes/appointmentForm/deleteItemAppointmentForm.ts
@@ -4,7 +4,7 @@ export const deleteItemAppointmentForm = async ( app: FastifyInstance) => {
     app.delete('/deleteItemAppointmentForm/:itemsAppointmentFormId', (request: FastifyRequest<{Params: {itemsAppointmentFormId: number}}>, response: FastifyReply) => {
         //@ts-ignore
         app.mysql.query(
-            `delete from items_appointment_form where id = '${request.params.itemsAppointmentFormId}}'`,
+            `delete from items_appointment_form where id = ${request.params.itemsAppointmentFormId}}`,
             function onResult(error : any, result : any){
                 if(error) return response.send(error);
                 response.send({"status": 200, "message": "Pergunta deletada com sucesso."});
